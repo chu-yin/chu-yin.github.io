38c5e7c262df4113ffb6f9f4b10bfb604e8487f9
chu-yin <julie51686@gmail.com>
2023-03-12 23:56:35 +0800

    first commit

---
+++ count.js
@@ -0,0 +1,7 @@
+
+window.addEventListener('again', function() {
+    const again = document.querySelector('.again');
+    const body = document.querySelector('body');
+    again.classList.add('hide');
+    body.style.display = 'block';
+});
