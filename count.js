f628bdb1898e22b36e9cdf1c323478a8ccf15696
chu-yin <91184619+chu-yin@users.noreply.github.com>
2023-03-12 23:48:04 +0800

    Delete count.js

--- count.js
+++
@@ -1,7 +0,0 @@
-
-window.addEventListener('again', function() {
-    const again = document.querySelector('.again');
-    const body = document.querySelector('body');
-    again.classList.add('hide');
-    body.style.display = 'block';
-});
